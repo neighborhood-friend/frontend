6bd4fc6e5b97f678de7365747a695ceade161960
Rita <editaahn@yahoo.com>
2021-10-17 18:03:04 +0900

    fix import error

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,9 +1,9 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
+import { MonoText } from '../common-ui/StyledText';
 
 import Colors from '../constants/Colors';
-import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
 export default function EditScreenInfo({ path }: { path: string }) {
