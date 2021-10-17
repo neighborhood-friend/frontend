4a85f22fcddd05f6cba2a455eeeeece44a1fa9e4
Rita <editaahn@yahoo.com>
2021-10-17 18:32:22 +0900

    add Screen style

---
+++ common-ui/Screen.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/native";
+import { View } from "react-native";
+
+export const Screen = styled(View)`
+  padding: 20px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
