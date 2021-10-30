4a254b78ec907d38d23c149bf4b2bbfcc049efc7
Rita <editaahn@yahoo.com>
2021-10-30 14:20:49 +0900

    redefine BasicButton

--- common-ui/BasicButton.tsx
+++ common-ui/BasicButton.tsx
@@ -1,31 +1,73 @@
 import styled from '@emotion/native';
 import React from 'react';
-import { UI_COLORS } from './UiColors';
+import { UiColors } from './UiColors';
 import { Kind } from './Kind';
 import { Body1Bold } from './StyledText';
 
-const ButtonColors = {
-  [Kind.Primary]: UI_COLORS.keyGreen,
-  [Kind.Secondary]: UI_COLORS.darkGray,
+type ButtonColorsType = {
+  fill: string;
+  font: string;
+  border?: string;
+};
+
+const ButtonColors: { [key in Kind]: ButtonColorsType } = {
+  [Kind.Primary]: {
+    fill: UiColors.green[400],
+    font: UiColors.white,
+  },
+  [Kind.Outline]: {
+    fill: 'transparent',
+    font: UiColors.black,
+    border: UiColors.gray[300],
+  },
 };
 
 type BasicButtonProps = {
   onPress?: () => any;
   kind: Kind;
   text: string;
+  full?: boolean;
+  round?: boolean;
 };
 
-export const BasicButton = ({ onPress, kind, text }: BasicButtonProps) => {
+export const BasicButton = ({
+  onPress,
+  kind,
+  text,
+  full = false,
+  round = false,
+}: BasicButtonProps) => {
   return (
-    <ButtonBox onPress={onPress} fill={ButtonColors[kind]}>
-      <Body1Bold>{text}</Body1Bold>
+    <ButtonBox
+      onPress={onPress}
+      fill={ButtonColors[kind].fill}
+      full={full}
+      round={round}
+      border={ButtonColors[kind]?.border}
+    >
+      <Body1Bold style={{ color: ButtonColors[kind].font }}>{text}</Body1Bold>
     </ButtonBox>
   );
 };
 
-const ButtonBox = styled.Pressable<{ fill: string }>`
+type ButtonBoxProps = {
+  fill: string;
+  border?: string;
+  full: boolean;
+  round: boolean;
+};
+
+const ButtonBox = styled.Pressable<ButtonBoxProps>`
   background-color: ${props => props.fill};
-  width: 100%;
+  ${props => props.full && 'width: 100%'};
+  ${props =>
+    props.border &&
+    `
+      borderWidth: 1px;
+      borderColor: ${props.border}
+  `};
+  padding: 12px 18px;
+  borderRadius: ${props => (props.round ? '24px' : '8px')};
   justify-content: center;
   align-items: center;
 `;
